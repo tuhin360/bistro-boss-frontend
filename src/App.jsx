ee99b08ea30c7d487e04f68db965fabb4d7a0772
tuhin <tuhin153695@gmail.com>
2025-05-19 17:10:40 +0600

    category swipper added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Bistro</h1>
-      <button className="btn">Button</button>
-<button className="btn btn-primary">Button</button>
-<button className="btn btn-secondary">Button</button>
-<button className="btn btn-accent">Button</button>
-<button className="btn btn-ghost">Button</button>
-<button className="btn btn-link">Button</button>
+  
     </>
   );
 }
---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,49 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+
+import slide1 from "../../../assets/home/slide1.jpg";
+import slide2 from "../../../assets/home/slide2.jpg";
+import slide3 from "../../../assets/home/slide3.jpg";
+import slide4 from "../../../assets/home/slide4.jpg";
+import slide5 from "../../../assets/home/slide5.jpg";
+
+const slides = [
+  { img: slide1, title: "Salads" },
+  { img: slide2, title: "Soups" },
+  { img: slide3, title: "Pizzas" },
+  { img: slide4, title: "Deserts" },
+  { img: slide5, title: "Salads" },
+];
+
+const Category = () => {
+  return (
+    <div className="mb-24">
+      <Swiper
+        slidesPerView={1}
+        breakpoints={{
+          640: { slidesPerView: 2 },
+          768: { slidesPerView: 3 },
+          1024: { slidesPerView: 4 },
+        }}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{ clickable: true }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        {slides.map((slide, index) => (
+          <SwiperSlide key={index} className="relative">
+            <img src={slide.img} alt={`${slide.title} image`} className="w-full" />
+            <h3 className="absolute bottom-5 left-1/2 transform -translate-x-1/2 text-2xl md:text-3xl lg:text-4xl uppercase text-white font-bold py-2">
+              {slide.title}
+            </h3>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Category;
