2db5cb5ecc656997113260c9ac37b12457fccfcb
tuhin <tuhin153695@gmail.com>
2025-08-03 18:27:38 +0600

    change localhost:5000 to vercel domains

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -146,7 +146,7 @@ export const router = createBrowserRouter(
           ),
           loader: ({ params }) =>
             fetch(
-              `http://localhost:5000/menu/${params.id}`
+              `https://bistro-boss-backend-rose.vercel.app/menu/${params.id}`
             ),
         },
       ],
